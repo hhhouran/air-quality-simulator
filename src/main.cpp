b782fb7c6fbcce655b17315135a34fd6389a9648
Hussein Hourani <hhhouran@umich.edu>
2025-01-19 08:47:02 -0500

    Fix readings Fix auto scroll behavior

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <LiquidCrystal.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+
 // The values below are initializing the connectors from the lcd to the board
 // We then instiantiate the LiquidCrystal object with the values
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -14,13 +15,14 @@ DHT dht = DHT(A0, DHT11);
 void setup()
 {
   // Begin method is configuring our rows/columns of our lcd
-  lcd.begin(8, 0);
-  humidity = dht.readHumidity();
+  humidity = dht.readHumidity(true);
   temperature = dht.readTemperature(true, false);
+  lcd.begin(16, 2);
 }
 
 void loop()
 {
+  lcd.setCursor(0, 0);
   lcd.print("Temp: " + String(temperature) + "F ");
   lcd.autoscroll();
   delay(2000);
